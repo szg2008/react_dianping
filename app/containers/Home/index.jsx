ce859c1f27befac76cda2b2c76fe198a9f0e4cad
szg2008 <l>
2017-07-27 09:14:16 +0800

    Ad

--- app/containers/Home/index.jsx
+++ app/containers/Home/index.jsx
@@ -4,6 +4,7 @@ import { bindActionCreators } from 'redux'
 import {connect} from 'react-redux'
 import HomeHeader from '../../components/HomeHeader'
 import Category from '../../components/Category'
+import Ad from './subpage/Ad'
 class Home extends React.Component {
     constructor(props, context) {
         super(props, context);
@@ -14,6 +15,8 @@ class Home extends React.Component {
             <div>
                 <HomeHeader cityName={this.props.userinfo.cityName}/>
                 <Category />
+                <div style={{'height':'15px'}}></div>
+                <Ad />
             </div>
         )
     }
---
+++ mock/home/ad.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        title: '暑假5折',
+        img: 'http://sns-img.b0.upaiyun.com/dunzd/1707/2709/02/150737409055541501117376.png',
+        link: 'http://www.baidu.com'
+    },
+    {
+        title: '特价出国',
+        img: 'http://sns-img.b0.upaiyun.com/dunzd/1707/2709/03/150451706863621501117410.png',
+        link: 'http://www.baidu.com'
+    },
+    {
+        title: '亮亮车',
+        img: 'http://sns-img.b0.upaiyun.com/dunzd/1707/2709/04/150796375827831501117445.png',
+        link: 'http://www.baidu.com'
+    },
+    {
+        title: '学钢琴',
+        img: 'http://sns-img.b0.upaiyun.com/dunzd/1707/2709/04/151080008514491501117468.png',
+        link: 'http://www.baidu.com'
+    },
+    {
+        title: '电影',
+        img: 'http://sns-img.b0.upaiyun.com/dunzd/1707/2709/04/150704949691641501117489.png',
+        link: 'http://www.baidu.com'
+    },
+    {
+        title: '旅游热线',
+        img: 'http://sns-img.b0.upaiyun.com/dunzd/1707/2709/05/150917293487191501117514.png',
+        link: 'http://www.baidu.com'
+    }
+]
