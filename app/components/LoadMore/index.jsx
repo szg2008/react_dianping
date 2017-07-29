8c77d2f0440da993dae92e6b596a8af5e9b5cee0
1039168735@qq.com <1039168735@qq.com>
2017-07-29 22:43:53 +0800

    LoadMore

---
+++ app/components/LoadMore/index.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import PureRenderMixin from 'react-addons-pure-render-mixin'
+
+import './style.less'
+class LoadMore extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+    }
+    loadMoreData(){
+        this.props.loadMoreFn()
+    }
+    render() {
+        return (
+            <div className="load-more" ref="wrapper">
+                {
+                    this.props.isLoadingMore
+                    ?<span>加载中...</span>
+                    :<span onClick={this.loadMoreData.bind(this)}>加载更多</span>
+                }
+            </div>
+        )
+    }
+    componentDidMount(){
+        let loadMoreFn = this.props.loadMoreFn
+        let wrapper = this.refs.wrapper
+
+        let timeoutId
+        window.addEventListener('scroll',function(){
+            if(this.props.isLoadingMore){
+                return
+            }
+            if(timeoutId){
+                clearTimeout(timeoutId)
+            }
+            timeoutId = setTimeout(()=>{
+                const top = wrapper.getBoundingClientRect().top
+                const windowHeight = window.screen.height
+                if(top && top < windowHeight){
+                    //当wrapper已经滚滚动到页面可视范围内的时候进行加载
+                    loadMoreFn();
+                }
+            },50)
+        }.bind(this),false)
+    }
+}
+
+module.exports = LoadMore
--- app/containers/Home/subpage/List.jsx
+++ app/containers/Home/subpage/List.jsx
@@ -2,21 +2,40 @@ import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
 import { getListData } from '../../../fetch/home/home.js'
 import ListComponent from '../../../components/List'
+import LoadMore from '../../../components/LoadMore'
 import './style.less'
 class List extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
         this.state = {
-            data:[],
-            hasMore:false
+            data:[],//存储列表信息
+            hasMore:false,//记录当前是否有更多数据需要加载
+            isLoadingMore:false,//记录当前状态下，是加载中还是点击加载更多
+            page:1,//下一页的页码
         }
     }
     //获取首平数据
     loadFirstPage(){
         const cityName = this.props.cityName
-        const result = getListData(cityName,1)
+        const result = getListData(cityName,0)
         this.resultHandle(result)
+    }
+    //加载更多数据
+    loadMoreData(){
+        // this.resultHandle()
+        this.setState({
+            isLoadingMore:true
+        })
+        const cityName = this.props.cityName
+        const page = this.state.page
+        const result = getListData(cityName,page)
+        this.resultHandle(result)
+
+        this.setState({
+            page:page + 1,
+            isLoadingMore:false
+        })
     }
     resultHandle(result){
         result.then((result)=>(result.json()))
@@ -24,7 +43,7 @@ class List extends React.Component {
             const hasMore = json.hasMore
             const data = json.data
             this.setState({
-                data:data,
+                data:this.state.data.concat(data),
                 hasMore:hasMore
             })
         })
@@ -43,6 +62,12 @@ class List extends React.Component {
                     ?<ListComponent data={this.state.data}/>
                     :'加载中...'
                 }
+                {
+                    this.state.hasMore
+                    ?<LoadMore isLoadingMore={this.state.isLoadingMore} loadMoreFn={this.loadMoreData.bind(this)}/>
+                    :''
+                }
+
 
             </div>
         )
