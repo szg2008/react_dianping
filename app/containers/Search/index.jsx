651778605e07483c5bd60c74d4b8122ebcad745b
1039168735@qq.com <1039168735@qq.com>
2017-07-30 22:29:32 +0800

    Search

--- app/containers/Search/index.jsx
+++ app/containers/Search/index.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
-
+import SearchHeader from '../../components/SearchHeader'
+import SearchList from './subpage/List.jsx'
 class Search extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
     }
     render() {
+        const params = this.props.params
         return (
             <div>
-                <h1>Search</h1>
+                <SearchHeader keyword={params.keyword}/>
+                <SearchList keyword={params.keyword} category={params.category}/>
             </div>
         )
-    }
-    componentDidMount(){
-        console.log(this.props.params)
     }
 }
 
